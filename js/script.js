d0157b4dd5d989a41a39fd4690cfbad776e99c5b
Juank628 <jsanchez_zunino@yahoo.com>
2019-06-03 18:00:58 -0500

    for community check

--- js/script.js
+++ js/script.js
@@ -10,8 +10,9 @@ let itemsPerPage = 10;
 
 /***
 showPage function:
-Show every item included in the page number
-Hide any other item
+Show every item included in the page number.
+In case of search only shows the search results in its according page.
+Hide any other item.
 ***/
 
 const showPage = (list, page, isSearch, searchName = "") => {
@@ -31,16 +32,14 @@ const showPage = (list, page, isSearch, searchName = "") => {
         } else {
           list[i].style.display = "none";
         }
-        searchArray.push(searchName);
+        searchArray.push(itemName);
         index++;
       } else {
         list[i].style.display = "none";
       }
     }
-    if (searchArray.length === 0) {
-      console.log("no items");
-    }
-    return searchArray;
+    return searchArray;    //return all the names found
+
   } else {
     for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
@@ -49,17 +48,17 @@ const showPage = (list, page, isSearch, searchName = "") => {
         list[i].style.display = "none";
       }
     }
-    return false;
+    return false;          //return false when not perform search operation
   }
 };
 
 /*** 
 appendPageLinks function: 
-Create the pages links
-Add events to each page link
+Delete the pages links if exists and create it again.
+Create no items found message if the list is empty.
 ***/
 
-const appendPageLinks = (list, isSearch) => {
+const appendPageLinks = (list, isSearch, searchName = "") => {
   const divPage = document.querySelector("div.page");
 
   if (document.querySelector(".pagination")) {
@@ -84,18 +83,19 @@ const appendPageLinks = (list, isSearch) => {
           document.querySelectorAll(".pagination ul li a")[i].className = "";
         }
         e.target.className = "active";
-        showPage(listItems, e.target.textContent, isSearch);
+        showPage(listItems, e.target.textContent, isSearch, searchName);
       });
       li.appendChild(a);
       ulLinks.appendChild(li);
     }
-
     ulLinks.firstChild.firstChild.className = "active";
     divLinks.appendChild(ulLinks);
     divPage.appendChild(divLinks);
+
   } else {
     noItemsMessage = document.createElement("p");
     noItemsMessage.textContent = "No items found";
+    noItemsMessage.style.color = "red";
     divLinks.appendChild(noItemsMessage);
     divPage.appendChild(divLinks);
   }
@@ -103,11 +103,17 @@ const appendPageLinks = (list, isSearch) => {
 
 /*** 
 appendSearchBar function:
-Create the search bar
+Create the search bar and its events.
+A search is performed when click the search button and when type in the search input.
+Every time a search is performed, the page links are updated.
 ***/
 const appendSearchBar = () => {
   const appendLinks = () => {
-    appendPageLinks(showPage(listItems, 1, true, searchInput.value), true);
+    appendPageLinks(
+      showPage(listItems, 1, true, searchInput.value),
+      true,
+      searchInput.value
+    );
   };
 
   const searchDiv = document.createElement("div");
@@ -130,8 +136,8 @@ const appendSearchBar = () => {
 /***
 When the page is loaded the first time:
 call the first page
-create the page links
 create the search bar
+create the page links
 ***/
 
 document.addEventListener("DOMContentLoaded", () => {
