6847032bb4b74c2fbc9339f6cdb28da4e1c3a052
Juank628 <jsanchez_zunino@yahoo.com>
2019-06-03 16:30:33 -0500

    extra credits no refactored

--- js/script.js
+++ js/script.js
@@ -2,89 +2,140 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/*Global variables*/
 
 const listItems = document.querySelector("ul.student-list").children;
 let itemsPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+/***
+showPage function:
+Show every item included in the page number
+Hide any other item
 ***/
 
-const showPage = (list, page) => {
-   let startIndex = (page * itemsPerPage) - itemsPerPage;
-   let endIndex = page * itemsPerPage;
+const showPage = (list, page, isSearch, searchName = "") => {
+  let startIndex = page * itemsPerPage - itemsPerPage;
+  let endIndex = page * itemsPerPage;
 
-   for(let i=0; i<list.length; i++){
-      if(i >= startIndex && i < endIndex){
-         list[i].style.display = "block";
+  if (isSearch) {
+    let itemName = "";
+    let index = 0;
+    let searchArray = [];
+
+    for (let i = 0; i < list.length; i++) {
+      itemName = document.querySelectorAll("ul.student-list h3")[i].textContent;
+      if (itemName.indexOf(searchName) !== -1) {
+        if (index >= startIndex && index < endIndex) {
+          list[i].style.display = "block";
+        } else {
+          list[i].style.display = "none";
+        }
+        searchArray.push(searchName);
+        index++;
       } else {
-         list[i].style.display = "none";
+        list[i].style.display = "none";
       }
-   }
-}
+    }
+    if (searchArray.length === 0) {
+      console.log("no items");
+    }
+    return searchArray;
+  } else {
+    for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+        list[i].style.display = "block";
+      } else {
+        list[i].style.display = "none";
+      }
+    }
+    return false;
+  }
+};
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+appendPageLinks function: 
+Create the pages links
+Add events to each page link
 ***/
 
-const appendPageLinks = list => {
-   let numberOfPages = Math.ceil(list.length/itemsPerPage);
-   const divPage = document.querySelector("div.page");
-   const divLinks = document.createElement("div");
-   divLinks.className = "pagination";
-   const ulLinks = document.createElement("ul");
+const appendPageLinks = (list, isSearch) => {
+  const divPage = document.querySelector("div.page");
 
-   for(let i=1; i<=numberOfPages; i++){
+  if (document.querySelector(".pagination")) {
+    divPage.removeChild(document.querySelector(".pagination"));
+  }
+
+  const divLinks = document.createElement("div");
+  divLinks.className = "pagination";
+
+  if (list.length > 0) {
+    let numberOfPages = Math.ceil(list.length / itemsPerPage);
+    const ulLinks = document.createElement("ul");
+
+    for (let i = 1; i <= numberOfPages; i++) {
       const li = document.createElement("li");
       const a = document.createElement("a");
       a.textContent = i;
       a.href = "#";
-      a.addEventListener('click',(e)=>{
-         e.preventDefault();
-         for(let i=0; i<numberOfPages; i++){
-            document.querySelectorAll(".pagination ul li a")[i].className = "";
-         }
-         e.target.className = "active"
-         showPage(listItems,e.target.textContent);
-      })
+      a.addEventListener("click", e => {
+        e.preventDefault();
+        for (let i = 0; i < numberOfPages; i++) {
+          document.querySelectorAll(".pagination ul li a")[i].className = "";
+        }
+        e.target.className = "active";
+        showPage(listItems, e.target.textContent, isSearch);
+      });
       li.appendChild(a);
       ulLinks.appendChild(li);
-   }
+    }
 
-   ulLinks.firstChild.firstChild.className = "active";
-   divLinks.appendChild(ulLinks);
-   divPage.appendChild(divLinks);
-}
+    ulLinks.firstChild.firstChild.className = "active";
+    divLinks.appendChild(ulLinks);
+    divPage.appendChild(divLinks);
+  } else {
+    noItemsMessage = document.createElement("p");
+    noItemsMessage.textContent = "No items found";
+    divLinks.appendChild(noItemsMessage);
+    divPage.appendChild(divLinks);
+  }
+};
 
-document.addEventListener('DOMContentLoaded', ()=>{
-   showPage(listItems, 1);
-   appendPageLinks(listItems);
-})
+/*** 
+appendSearchBar function:
+Create the search bar
+***/
+const appendSearchBar = () => {
+  const appendLinks = () => {
+    appendPageLinks(showPage(listItems, 1, true, searchInput.value), true);
+  };
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+  const searchDiv = document.createElement("div");
+  const searchInput = document.createElement("input");
+  const searchBotton = document.createElement("button");
+
+  searchDiv.className = "student-search";
+  searchInput.placeholder = "Search for students...";
+  searchBotton.textContent = "Search";
+
+  searchBotton.addEventListener("click", () => appendLinks());
+  searchInput.addEventListener("keyup", () => appendLinks());
+
+  searchDiv.appendChild(searchInput);
+  searchDiv.appendChild(searchBotton);
+
+  document.getElementsByClassName("page-header")[0].appendChild(searchDiv);
+};
+
+/***
+When the page is loaded the first time:
+call the first page
+create the page links
+create the search bar
+***/
+
+document.addEventListener("DOMContentLoaded", () => {
+  showPage(listItems, 1, false);
+  appendSearchBar();
+  appendPageLinks(listItems, false);
+});
